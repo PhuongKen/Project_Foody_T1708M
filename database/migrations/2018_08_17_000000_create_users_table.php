4fcbe34dd15508bb4c91dd21295f7f38b7085c1a
khainqd <khainqd00575@fpt.edu.vn>
2018-08-18 08:49:02 +0700

    Tao_controller_user

--- database/migrations/2018_08_17_000000_create_users_table.php
+++ database/migrations/2018_08_17_000000_create_users_table.php
@@ -15,9 +15,17 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('addressID')->unsigned();
+            $table->foreign('addressID')->references('id')->on('addresses');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('avartar');
+            $table->integer('phone');
+            $table->integer('gender')->default(0);// 0 male, 1 female, 2 other
+            $table->integer('status')->default(1); // 0 delete,  1 active, 2 block,
+            $table->integer('verifyEmail')->default(0); // 0 chưa xác nhận, 1 đã xác nhận.
+            $table->integer('role')->default(1); // 1 client, 2 admin
             $table->rememberToken();
             $table->timestamps();
         });
