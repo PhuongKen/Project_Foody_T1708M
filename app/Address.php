4170b45c357f9f0dcf3cf6a6f912a338065dce0e
Your Name <you@example.com>
2018-08-20 20:32:14 +0700

    Thêm lọc địa chỉ theo id

--- app/Address.php
+++ app/Address.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $table = 'addresses';
+    public function provind(){
+        return $this->hasOne('App\Provind','provindID','id');
+    }
+    public function district(){
+        return $this->hasOne('App\District','districtID','id');
+    }
+    public function ward(){
+        return $this->hasOne('App\Ward','wardID','id');
+    }
+    public function restaurant(){
+        return $this->belongsTo('App\Restaurant','addressID','id');
+    }
 }
