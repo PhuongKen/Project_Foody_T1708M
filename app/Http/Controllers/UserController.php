6f4dc21dc373f05cef72dd923121d841d9c2be20
khainqd <khainqd00575@fpt.edu.vn>
2018-08-20 16:00:22 +0700

    create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,10 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        $provind = Provind::all();
+        $district = District::all();
+        $ward = Ward::all();
+        return view('admin.user.form', compact('provind', 'district', 'ward'));
     }
 
     /**
@@ -34,7 +37,39 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'avartar' => 'required',
+            'phone' => 'required',
+
+        ],
+            [
+                'name.required' => 'Bạn chưa nhập tên',
+                'email.required' => 'Bạn chưa nhập email',
+                'password.required' => 'Bạn chưa nhập mật khẩu',
+                'avartar.required' => 'Bạn chưa nhập ảnh đại diện',
+                'phone.required' => 'Bạn chưa nhập số điện thoại',
+            ]
+        );
+
+
+        $user = new User();
+        $address = new Address();
+        $address -> provindID = Input::get('provind');
+        $address -> districtID = Input::get('district');
+        $address -> wardID = Input::get('ward');
+        $address -> save();
+        $user -> addressID = $address-> id;
+        $user -> name = Input::get('name');
+        $user -> email = Input::get('email');
+        $user -> password = Hash::make(Input::get('password'));
+        $user -> avartar = Input::get('avartar');
+        $user -> phone = Input::get('phone');
+        $user -> status = Input::get('status');
+        $user -> save();
+        return redirect('/admin/user');
     }
 
     /**
