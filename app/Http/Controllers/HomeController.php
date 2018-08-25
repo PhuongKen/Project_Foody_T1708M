4c4b7e1288f8df906ce7defe937bce4b38e22c1a
longdnd00573 <longdnd00573@fpt.edu.vn>
2018-08-25 12:30:06 +0700

    Chinh sua list cho category

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,8 @@ class HomeController extends Controller
 
     public function getRegister()
     {
-        return view('client.register');
+        $categories = Category::all();
+        return view('client.register',compact('categories'));
     }
 
     public function postRegister(Request $req)
@@ -79,17 +80,19 @@ class HomeController extends Controller
 
     public function getLogin()
     {
+        $categories = Category::all();
         if(Auth::check()){
-            return view('client.index');
+            return view('client.index',compact('categories'));
         }else{
-            return view('client.login');
+            return view('client.login',compact('categories'));
         }
     }
 
     public function postLogin(Request $req)
     {
+        $categories = Category::all();
         if (Auth::attempt(['email' => $req->email, 'password' => $req->password, 'verifyEmail' => 1])) {
-            return view('client.index');
+            return view('client.index',compact('categories'));
         }else{
             return redirect()->back()->with('thatbai','Sai thông tin đăng nhập');
         }
