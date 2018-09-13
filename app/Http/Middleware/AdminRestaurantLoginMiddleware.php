56dcdb14845b1a9291f3cd0f33a1d4c793910fb1
Your Name <you@example.com>
2018-09-13 10:40:16 +0700

    sua dang nhap admin

--- app/Http/Middleware/AdminRestaurantLoginMiddleware.php
+++ app/Http/Middleware/AdminRestaurantLoginMiddleware.php
@@ -10,17 +10,18 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+
 class AdminRestaurantLoginMiddleware
 {
     public function handle($request, Closure $next)
     {
-        if(!Auth::check()){
+        $user = Auth::user();
+        if (!Auth::check()) {
             return redirect('/dang-nhap-admin-restaurant');
-        }else{
-            $user = Auth::user();
-            if($user->role == 2){
+        } else {
+            if ($user->role == 2) {
                 return $next($request);
-            }else{
+            } else {
                 return redirect('/dang-nhap-admin-restaurant');
             }
         }
